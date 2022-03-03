7043513af528b49c0fb31918aa869d65e81dd796
mszak10 <49935726+mszak10@users.noreply.github.com>
2022-03-03 18:55:17 +0100

    faultyCoinToss.py (example from the book)

---
+++ Chapter 11 - Debugging/faultyCoinToss.py
@@ -0,0 +1,19 @@
+# Example of a program to fix from the book
+# It is fixed in fixedCoinToss.py file
+
+import random
+
+guess = ""
+while guess not in ("heads", "tails"):
+    print("Guess the coin toss! Enter heads or tails:")
+    guess = input()
+toss = random.randint(0, 1)  # 0 is tails, 1 is heads
+if toss == guess:
+    print("You got it!")
+else:
+    print("Nope! Guess again!")
+    guesss = input()
+    if toss == guess:
+        print("You got it!")
+    else:
+        print("Nope. You are really bad at this game.")
