9d327b3cf6de26fcb87dcdd501e7107264a28ab7
mszak10 <49935726+mszak10@users.noreply.github.com>
2023-05-13 11:45:35 +0200

    Finally, after a year, found a solution to bypass the 'Before you continue to Google' screen

--- Chapter 12 - Web Scraping/openSearchResults.py
+++ Chapter 12 - Web Scraping/openSearchResults.py
@@ -1,23 +1,23 @@
 # openSearchResults.py - Opens several search results.
-# TODO: To be fixed - passing "Before you continue to Google Search" window
-
+# from lib2to3.pgen2 import driver
+# from selenium.webdriver.common.by import By
 import bs4
-import os
 import pyinputplus as pyip
 import requests
 import webbrowser
 
+
 inp = pyip.inputStr(prompt="What are you looking for? ")
 print('Searching...')
-res = requests.get('https://google.com/search?q=' + ' '.join(inp))
+headers = {"User-Agent": "Mozilla/5.0"}
+cookies = {"CONSENT": "YES+cb.20210720-07-p0.en+FX+410"}
+res = requests.get('https://google.com/search?q=' + ' '.join(inp), headers=headers, cookies=cookies)
 res.raise_for_status()
 print(f"Status: {res.status_code}")
 
 # Retrieve top search result links.
 soup = bs4.BeautifulSoup(res.text, 'html.parser')
-print(soup)  # output the css and html (temporarly for debugging)
-# Open a browser tab for each result.
-linkElems = soup.select('.package-snippet')
+linkElems = soup.select('.BVG0Nb')  # works as of May 13, 2023
 
 numOpen = min(5, len(linkElems))
 for i in range(numOpen):
